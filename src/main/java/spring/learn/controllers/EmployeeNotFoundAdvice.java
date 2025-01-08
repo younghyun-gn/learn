27ef83297b4ed8fa8353e3af8a8d4c6b0fed18c6
younghyun-gn <younghyun.chi@genesisnest.com>
2025-01-08 15:28:57 +0900

    feature: basic error handling

---
+++ src/main/java/spring/learn/controllers/EmployeeNotFoundAdvice.java
@@ -0,0 +1,16 @@
+package spring.learn.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// @RestControllerAdvice deals with how to return HTTP error responses
+@RestControllerAdvice
+public class EmployeeNotFoundAdvice {
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String employeeNotFoundHandler(EmployeeNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
